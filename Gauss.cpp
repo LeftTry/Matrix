cdd4ae8b057299e6adb9b286dee578b8956ccbb3
LeftTry <lerest.go@gmail.com>
2021-10-06 10:51:50 +0300

    gaussDebug

---
+++ Gauss.cpp
@@ -0,0 +1,7 @@
+#include "Matrix.h"
+#include <vector>
+
+using namespace std;
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include "Matrix.h"
 #include <vector>
+#include "Gauss.cpp"
 
 using namespace std;
 
+void gauss(Matrix<double> a, vector<double> &b){
+    for(int i = 0;i < a.getn();i++){
+        for(int j = 0;j < a.getn();j++){
+            a.M[i][j] /= a.M[i][i];
+        }
+        b[i] /= a.M[i][i];
+        cout << b[i] << endl;
+        for(int j = 0;j < a.getn();j++){
+            int x = a.M[j][i];
+            for(int k = 0;k < a.getn();k++){
+                if(j != i) {
+                    a.M[j][k] -= (x / a.M[i][i]) * a.M[i][k];
+                    //cout << a.M[j][k]<< endl;
+                }
+            }
+            if(j != i)
+                b[j] -= (x / a.M[i][i]) * b[i];
+            //cout << b[j] << endl;
+        }
+    }
+    cout << a;
+    for(auto i : b) cout << i << " ";
+    cout << endl;
+    //return b;
+}
+
 int main() {
-    cout << "You need to enter 2 Matrix:" << endl;
+    /*cout << "You need to enter 2 Matrix:" << endl;
     int n, m;
     cin >> n >> m;
     Matrix<int> a(n, m);
@@ -31,6 +58,13 @@ int main() {
     try{
         auto d = deter(n, n, b);
         cout << d << endl;
-    } catch (const char* s) {cerr << s << endl;}
+    } catch (const char* s) {cerr << s << endl;}*/
+    int n, m;
+    cin >> n >> m;
+    vector<vector<double>> V(n, vector<double>(m));
+    vector<double> ans(n);
+    for(int i = 0;i < n;i++){ for(int j = 0;j < m;j++) cin >> V[i][j]; cin >> ans[i];}
+    Matrix<double> b(V);
+    gauss(b, ans);
     return 0;
 }
