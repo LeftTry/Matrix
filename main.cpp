76ba43e1883ff636839406f2116efde7ed878472
LeftTry <lerest.go@gmail.com>
2021-10-09 10:10:05 +0300

    Added inconsistent system finder

--- main.cpp
+++ main.cpp
@@ -17,27 +17,25 @@ void gauss(Matrix<double> a, vector<double> &b){
             for(int k = 0;k < a.getn();k++){
                 if(j != i) {
                     a.M[j][k] -= (x / a.M[i][i]) * a.M[i][k];
-                    //cout << a.M[j][k]<< endl;
                 }
             }
             if(j != i)
                 b[j] -= (x / a.M[i][i]) * b[i];
-            //cout << b[j] << endl;
         }
     }
-    //cout << a;
     for(auto i : b){ if(!isnan(i))cout << i << " "; else{ cout << "System doesn't have the solution" << endl; break;}}
     cout << endl;
-    //return b;
 }
 
 void non_square_gauss(Matrix<double> a, vector<double> &b){
+    //cout << "ok" << endl;
     for(int i = 0;i < a.getn();i++){
         //for(int j = 0;j < a.getn();j++){
             //a.M[i][j] /= a.M[i][i];
         //}
         //b[i] /= a.M[i][i];
         //cout << b[i] << endl;
+        if(i >= a.getm()) break;
         for(int j = 0;j < a.getn();j++){
             int x = a.M[j][i];
             for(int k = 0;k < a.getm();k++){
@@ -49,6 +47,18 @@ void non_square_gauss(Matrix<double> a, vector<double> &b){
             if(j != i)
                 b[j] -= (x / a.M[i][i]) * b[i];
             //cout << b[j] << endl;
+        }
+    }
+    //cout << "ok" << endl;
+    for(int i = 0;i < a.getn();i++){
+        for(int j = 0;j < a.getm();j++){
+            if(a.M[i][j] != 0)
+                break;
+            //cout << b[i] << endl;
+            if(j == a.getm() - 1 && a.M[i][j] == 0 && b[i] != 0) {
+                cout << "System is inconsistent" << endl;
+                return;
+            }
         }
     }
     cout << a;
@@ -106,6 +116,8 @@ int main() {
     vector<double> ans(n);
     for(int i = 0;i < n;i++){ for(int j = 0;j < m;j++) cin >> V[i][j]; cin >> ans[i];}
     Matrix<double> b(V);
+    //Matrix<double> b(n, m);
+    //b.M = V;
     non_square_gauss(b, ans);
     return 0;
 }
