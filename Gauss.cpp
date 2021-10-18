4be3e1ff803d4270f9fb6068db7ba6c862ee3007
Иван Александрович Стахов <50211739+LeftTry@users.noreply.github.com>
2021-10-18 21:18:36 +0300

    Fixed bugs

--- Gauss.cpp
+++ Gauss.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include "Matrix.h"
 #include <vector>
-#include <fstream>
 
 using namespace std;
-
-//ofstream fout("output.txt");
 
 void gauss(Matrix<double>& a, vector<double> &b){
     for(int i = 0;i < a.getN(); i++){
--- main.cpp
+++ main.cpp
@@ -8,21 +8,21 @@
 using namespace std;
 
 int main() {
-    //ifstream fin("input.txt");
+    ifstream fin("input.txt");
     int k, i = 1;
     string s;
     //getline(fin, s);
     //k = atoi( s.c_str() );
-    k = 1;
+    fin >> k;
     while(k != 0) {
         int n, m;
         //getline(fin, s);
-        cin >> n >> m;
+        fin >> n >> m;
         vector<vector<double>> V(n, vector<double>(m));
         vector<double> ans(n);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) cin >> V[i][j];
-            cin >> ans[i];
+            for (int j = 0; j < m; j++) fin >> V[i][j];
+            fin >> ans[i];
         }
         Matrix<double> b(V);
         cout << "Case#" << i << ":" << endl;
